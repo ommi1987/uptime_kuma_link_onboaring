fad0aaced862eccc0e558aa43c19f479e350772e
ommi1987 <159000120+ommi1987@users.noreply.github.com>
2025-09-09 11:13:07 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,4 @@
-
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))      # integer
+height = float(input("Enter your height: "))  # decimal
+print("your name is ", name, "You are", age, "years old and", height, "meters tall.")
