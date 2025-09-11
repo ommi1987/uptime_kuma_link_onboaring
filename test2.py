ae0f6c5e0f374dae8bb0389398b6eead0b37bd05
ommi1987 <159000120+ommi1987@users.noreply.github.com>
2025-09-11 22:54:53 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -11,6 +11,7 @@ hostname = sys.argv[2]
 
 URL='http://192.168.101.13:3001//dashboard'
 with UptimeKumaApi(URL) as api:
+  api.login(github_user, github_pass)
   notifications = api.get_notifications()
   # Find the one you want (by name or other property)
   webhook_name = "Uptime Kuma ISP Alert"  # existing webhook name
@@ -18,6 +19,5 @@ with UptimeKumaApi(URL) as api:
   for n in notifications:
     if n["name"] == webhook_name:
         notif_id = n["id"]
-        break  
-  api.login(github_user, github_pass)
+        break    
   api.add_monitor(type='ping', name=name, hostname=hostname, notificationIDList=notif_id)
