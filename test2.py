10737f76580c1ae0f5aafdf69a7584471cf94774
ommi1987 <159000120+ommi1987@users.noreply.github.com>
2025-09-11 15:37:46 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,15 @@
 from uptime_kuma_api import UptimeKumaApi
 import time
 import os
+import sys
 
 github_user = os.getenv("GITHUB_USER")
 github_pass = os.getenv("GITHUB_PASS")
 
+name = sys.argv[1]
+hostname = sys.argv[2]
+
 URL='http://192.168.101.13:3001//dashboard'
 with UptimeKumaApi(URL) as api:
   api.login(github_user, github_pass)
-  api.add_monitor(type='ping', name=col2[row].value, hostname=col1[row].value)
+  api.add_monitor(type='ping', name=name, hostname=hostname)
