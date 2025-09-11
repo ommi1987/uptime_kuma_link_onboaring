6115a8e1a6030fa66bd44d857945a948d65f806c
ommi1987 <159000120+ommi1987@users.noreply.github.com>
2025-09-11 11:52:17 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))      # integer
-height = float(input("Enter your height: "))  # decimal
-print("your name is ", name, "You are", age, "years old and", height, "meters tall.")
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+
+print(f"Name: {name}, Age: {age}")
